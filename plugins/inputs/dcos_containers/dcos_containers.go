d38f84377948dddb8db9ae9b8ee5ad6e1a4d1784
Philip Norman <philip@mesosphere.io>
2018-07-06 11:30:54 -0700

    Use mesos-go to pull container data

--- plugins/inputs/dcos_containers/dcos_containers.go
+++ plugins/inputs/dcos_containers/dcos_containers.go
@@ -1,79 +1,26 @@
 package dcos_containers
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"context"
+	"fmt"
+	"io"
 	"log"
 	"math"
-	"net/http"
 	"time"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
+
+	"github.com/mesos/mesos-go/api/v1/lib/agent"
+	"github.com/mesos/mesos-go/api/v1/lib/agent/calls"
+	"github.com/mesos/mesos-go/api/v1/lib/httpcli"
+	"github.com/mesos/mesos-go/api/v1/lib/httpcli/httpagent"
 )
 
 const sampleConfig = `
 ## The URL of the local mesos agent
 agent_url = "http://127.0.0.1:5051"
 `
-
-// AgentStats describes the resource consumption of a mesos container
-type AgentStats struct {
-	// TODO: container_id
-	ExecutorID   string `json:"executor_id"`
-	ExecutorName string `json:"executor_name"`
-	FrameworkID  string `json:"framework_id"`
-	Source       string `json:"source"`
-	Statistics   struct {
-		CpusLimit             float64 `json:"cpus_limit"`
-		CpusNrPeriods         int64   `json:"cpus_nr_periods"`
-		CpusNrThrottled       int64   `json:"cpus_nr_throttled"`
-		CpusSystemTimeSecs    float64 `json:"cpus_system_time_secs"`
-		CpusThrottledTimeSecs float64 `json:"cpus_throttled_time_secs"`
-		CpusUserTimeSecs      float64 `json:"cpus_user_time_secs"`
-		MemAnonBytes          int64   `json:"mem_anon_bytes"`
-		MemFileBytes          int64   `json:"mem_file_bytes"`
-		MemLimitBytes         int64   `json:"mem_limit_bytes"`
-		MemMappedFileBytes    int64   `json:"mem_mapped_file_bytes"`
-		MemRssBytes           int64   `json:"mem_rss_bytes"`
-		Timestamp             float64 `json:"timestamp"`
-	} `json:"statistics"`
-	// TODO: blkio
-}
-
-// getFields flattens the statistics in AgentStats to a map
-func (as *AgentStats) getFields() map[string]interface{} {
-	// TODO detect nil results and don't add them to this map
-	results := make(map[string]interface{})
-	results["cpus_limit"] = as.Statistics.CpusLimit
-	results["cpus_nr_periods"] = as.Statistics.CpusNrPeriods
-	results["cpus_nr_throttled"] = as.Statistics.CpusNrThrottled
-	results["cpus_system_time_secs"] = as.Statistics.CpusSystemTimeSecs
-	results["cpus_throttled_time_secs"] = as.Statistics.CpusThrottledTimeSecs
-	results["cpus_user_time_secs"] = as.Statistics.CpusUserTimeSecs
-	results["mem_anon_bytes"] = as.Statistics.MemAnonBytes
-	results["mem_file_bytes"] = as.Statistics.MemFileBytes
-	results["mem_limit_bytes"] = as.Statistics.MemLimitBytes
-	results["mem_mapped_file_bytes"] = as.Statistics.MemMappedFileBytes
-	results["mem_rss_bytes"] = as.Statistics.MemRssBytes
-
-	return results
-}
-
-// getTags extracts relevant metadata in AgentStats to a map
-func (as *AgentStats) getTags() map[string]string {
-	results := make(map[string]string)
-	results["executor_id"] = as.ExecutorID
-	results["executor_name"] = as.ExecutorName
-	results["framework_id"] = as.FrameworkID
-	results["source"] = as.Source
-	return results
-}
-
-// getTimestamp returns the timestamp as a time rounded to the second
-func (as *AgentStats) getTimestamp() time.Time {
-	return time.Unix(int64(math.Trunc(as.Statistics.Timestamp)), 0)
-}
 
 // DCOSContainers describes the options available to this plugin
 type DCOSContainers struct {
@@ -94,21 +41,75 @@ func (dc *DCOSContainers) Description() string {
 // It is invoked on a schedule (default every 10s) by the telegraf runtime.
 func (dc *DCOSContainers) Gather(acc telegraf.Accumulator) error {
 	// TODO: timeout
-	// TODO: use /containers instead
 	// TODO: error handling
-	resp, _ := http.Get(dc.AgentUrl + "/monitor/statistics")
-	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	uri := dc.AgentUrl + "/api/v1"
+	cli := httpagent.NewSender(httpcli.New(httpcli.Endpoint(uri)).Send)
+	ctx := context.Background()
 
-	stats := []AgentStats{}
-	json.Unmarshal(body, &stats)
+	resp, err := cli.Send(ctx, calls.NonStreaming(calls.GetContainers()))
 
-	for _, as := range stats {
-		acc.AddFields("dcos_containers", as.getFields(), as.getTags(), as.getTimestamp())
+	defer func() {
+		if resp != nil {
+			resp.Close()
+		}
+	}()
+	if err != nil {
+		return err
+	}
+	for {
+		var r agent.Response
+		if err := resp.Decode(&r); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if t := r.GetType(); t == agent.Response_GET_CONTAINERS {
+			gc := r.GetGetContainers()
+			for _, c := range gc.Containers {
+				acc.AddFields("dcos_containers", cFields(c), cTags(c), cTS(c))
+			}
+		} else {
+			// TODO better error
+			fmt.Println("not getcontainers", t, r)
+		}
 	}
 
 	return nil
+}
+
+// cFields flattens a Container object into a map of metric labels and values
+func cFields(c agent.Response_GetContainers_Container) map[string]interface{} {
+	results := make(map[string]interface{})
+	rs := c.ResourceStatistics
+	results["cpus_limit"] = *rs.CPUsLimit
+	results["cpus_nr_periods"] = *rs.CPUsNrPeriods
+	results["cpus_nr_throttled"] = *rs.CPUsNrThrottled
+	results["cpus_system_time_secs"] = *rs.CPUsSystemTimeSecs
+	results["cpus_throttled_time_secs"] = *rs.CPUsThrottledTimeSecs
+	results["cpus_user_time_secs"] = *rs.CPUsUserTimeSecs
+	results["mem_anon_bytes"] = *rs.MemAnonBytes
+	results["mem_file_bytes"] = *rs.MemFileBytes
+	results["mem_limit_bytes"] = *rs.MemLimitBytes
+	results["mem_mapped_file_bytes"] = *rs.MemMappedFileBytes
+	results["mem_rss_bytes"] = *rs.MemRSSBytes
+	return results
+}
+
+// cTags extracts relevant metadata from a Container object as a map of tags
+func cTags(c agent.Response_GetContainers_Container) map[string]string {
+	results := make(map[string]string)
+	// results["service_name"] = *c.FrameworkName
+	results["executor_name"] = *c.ExecutorName
+	// results["task_name"] = *rs.TaskName
+	return results
+}
+
+// cTS retrieves the timestamp from a Container object as a time rounded to the
+// nearest second
+func cTS(c agent.Response_GetContainers_Container) time.Time {
+	return time.Unix(int64(math.Trunc(c.ResourceStatistics.Timestamp)), 0)
 }
 
 // init is called once when telegraf starts
