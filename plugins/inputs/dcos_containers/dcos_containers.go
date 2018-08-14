020c793d43cead83f895e35da1d9a37c5a68eac3
Philip Norman <philip@mesosphere.io>
2018-08-14 16:42:32 -0700

    Account for all basic ResourceStatistic fields

--- plugins/inputs/dcos_containers/dcos_containers.go
+++ plugins/inputs/dcos_containers/dcos_containers.go
@@ -107,19 +107,57 @@ func processResponse(resp mesos.Response, t agent.Response_Type) (agent.Response
 // cFields flattens a Container object into a map of metric labels and values
 func cFields(c agent.Response_GetContainers_Container) map[string]interface{} {
 	results := make(map[string]interface{})
-	// TODO account for all possible fields in ResourceStatistics
 	rs := c.ResourceStatistics
-	results["cpus_limit"] = *rs.CPUsLimit
-	results["cpus_nr_periods"] = *rs.CPUsNrPeriods
-	results["cpus_nr_throttled"] = *rs.CPUsNrThrottled
-	results["cpus_system_time_secs"] = *rs.CPUsSystemTimeSecs
-	results["cpus_throttled_time_secs"] = *rs.CPUsThrottledTimeSecs
-	results["cpus_user_time_secs"] = *rs.CPUsUserTimeSecs
-	results["mem_anon_bytes"] = *rs.MemAnonBytes
-	results["mem_file_bytes"] = *rs.MemFileBytes
-	results["mem_limit_bytes"] = *rs.MemLimitBytes
-	results["mem_mapped_file_bytes"] = *rs.MemMappedFileBytes
-	results["mem_rss_bytes"] = *rs.MemRSSBytes
+
+	// These items are not in alphabetical order; instead we preserve the order
+	// in the source of the ResourceStatistics struct to make it easy to update.
+	warnIfNotSet(setIfNotNil(results, "processes", rs.GetProcesses))
+	warnIfNotSet(setIfNotNil(results, "threads", rs.GetThreads))
+
+	warnIfNotSet(setIfNotNil(results, "cpus_user_time_secs", rs.GetCPUsUserTimeSecs))
+	warnIfNotSet(setIfNotNil(results, "cpus_system_time_secs", rs.GetCPUsSystemTimeSecs))
+	warnIfNotSet(setIfNotNil(results, "cpus_limit", rs.GetCPUsLimit))
+	warnIfNotSet(setIfNotNil(results, "cpus_nr_periods", rs.GetCPUsNrPeriods))
+	warnIfNotSet(setIfNotNil(results, "cpus_nr_throttled", rs.GetCPUsNrThrottled))
+	warnIfNotSet(setIfNotNil(results, "cpus_throttled_time_secs", rs.GetCPUsThrottledTimeSecs))
+
+	warnIfNotSet(setIfNotNil(results, "mem_total_bytes", rs.GetMemTotalBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_total_memsw_bytes", rs.GetMemTotalMemswBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_limit_bytes", rs.GetMemLimitBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_soft_limit_bytes", rs.GetMemSoftLimitBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_file_bytes", rs.GetMemFileBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_anon_bytes", rs.GetMemAnonBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_cache_bytes", rs.GetMemCacheBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_rss_bytes", rs.GetMemRSSBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_mapped_file_bytes", rs.GetMemMappedFileBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_swap_bytes", rs.GetMemSwapBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_unevictable_bytes", rs.GetMemUnevictableBytes))
+	warnIfNotSet(setIfNotNil(results, "mem_low_pressure_counter", rs.GetMemLowPressureCounter))
+	warnIfNotSet(setIfNotNil(results, "mem_medium_pressure_counter", rs.GetMemMediumPressureCounter))
+	warnIfNotSet(setIfNotNil(results, "mem_critical_pressure_counter", rs.GetMemCriticalPressureCounter))
+
+	warnIfNotSet(setIfNotNil(results, "disk_limit_bytes", rs.GetDiskLimitBytes))
+	warnIfNotSet(setIfNotNil(results, "disk_used_bytes", rs.GetDiskUsedBytes))
+	// TODO: Disk statistics (*rs.DiskStatistics)
+	// TODO: Blkio statistics (*rs.BlkioStatistics)
+	// TODO: Perf statistics (*rs.Perf)
+	warnIfNotSet(setIfNotNil(results, "net_rx_packets", rs.GetNetRxPackets))
+	warnIfNotSet(setIfNotNil(results, "net_rx_bytes", rs.GetNetRxBytes))
+	warnIfNotSet(setIfNotNil(results, "net_rx_errors", rs.GetNetRxErrors))
+	warnIfNotSet(setIfNotNil(results, "net_rx_dropped", rs.GetNetRxDropped))
+	warnIfNotSet(setIfNotNil(results, "net_tx_packets", rs.GetNetTxPackets))
+	warnIfNotSet(setIfNotNil(results, "net_tx_bytes", rs.GetNetTxBytes))
+	warnIfNotSet(setIfNotNil(results, "net_tx_errors", rs.GetNetTxErrors))
+	warnIfNotSet(setIfNotNil(results, "net_tx_dropped", rs.GetNetTxDropped))
+	warnIfNotSet(setIfNotNil(results, "net_tcp_rtt_microsecs_p50", rs.GetNetTCPRttMicrosecsP50))
+	warnIfNotSet(setIfNotNil(results, "net_tcp_rtt_microsecs_p90", rs.GetNetTCPRttMicrosecsP90))
+	warnIfNotSet(setIfNotNil(results, "net_tcp_rtt_microsecs_p95", rs.GetNetTCPRttMicrosecsP95))
+	warnIfNotSet(setIfNotNil(results, "net_tcp_rtt_microsecs_p99", rs.GetNetTCPRttMicrosecsP99))
+	warnIfNotSet(setIfNotNil(results, "net_tcp_active_connections", rs.GetNetTCPActiveConnections))
+	warnIfNotSet(setIfNotNil(results, "net_tcp_time_wait_connections", rs.GetNetTCPTimeWaitConnections))
+	// TODO: Net traffic control statistics (*rs.NetTrafficControlStatistics)
+	// TODO: Net snmp statistics (*rs.NetSNMPStatistics)
+
 	return results
 }
 
@@ -132,6 +170,42 @@ func cTags(c agent.Response_GetContainers_Container) map[string]string {
 // nearest second
 func cTS(c agent.Response_GetContainers_Container) time.Time {
 	return time.Unix(int64(math.Trunc(c.ResourceStatistics.Timestamp)), 0)
+}
+
+// setIfNotNil runs get() and adds its value to a map, if not nil
+func setIfNotNil(target map[string]interface{}, key string, get interface{}) error {
+	var val interface{}
+	var zero interface{}
+
+	switch get.(type) {
+	case func() uint32:
+		val = get.(func() uint32)()
+		zero = uint32(0)
+		break
+	case func() uint64:
+		val = get.(func() uint64)()
+		zero = uint64(0)
+		break
+	case func() float64:
+		val = get.(func() float64)()
+		zero = float64(0)
+		break
+	default:
+		return fmt.Errorf("get function for key %s was not of a recognized type", key)
+	}
+	// Zero is nil for numeric types
+	if val != zero {
+		target[key] = val
+	}
+	return nil
+}
+
+// warnIfNotSet is a convenience method to log a warning whenever setIfNotNil
+// did not succesfully complete
+func warnIfNotSet(err error) {
+	if err != nil {
+		log.Printf("Warning: %s", err)
+	}
 }
 
 // init is called once when telegraf starts
--- plugins/inputs/dcos_containers/dcos_containers_test.go
+++ plugins/inputs/dcos_containers/dcos_containers_test.go
@@ -100,6 +100,55 @@ func TestGather(t *testing.T) {
 	}
 }
 
+func TestSetIfNotNil(t *testing.T) {
+	t.Run("Legal set methods which return concrete values", func(t *testing.T) {
+		mmap := make(map[string]interface{})
+		methods := map[string]interface{}{
+			"a": func() uint32 { return 1 },
+			"b": func() uint64 { return 1 },
+			"c": func() float64 { return 1 },
+		}
+		expected := map[string]interface{}{
+			"a": uint32(1),
+			"b": uint64(1),
+			"c": float64(1),
+		}
+		for key, set := range methods {
+			err := setIfNotNil(mmap, key, set)
+			assert.Nil(t, err)
+		}
+		assert.Equal(t, mmap, expected)
+	})
+	t.Run("Legal set methods which return nil", func(t *testing.T) {
+		mmap := make(map[string]interface{})
+		methods := map[string]interface{}{
+			"a": func() uint32 { return 0 },
+			"b": func() uint64 { return 0 },
+			"c": func() float64 { return 0 },
+		}
+		expected := map[string]interface{}{}
+		for key, set := range methods {
+			err := setIfNotNil(mmap, key, set)
+			assert.Nil(t, err)
+		}
+		assert.Equal(t, mmap, expected)
+	})
+	t.Run("Illegal set methods", func(t *testing.T) {
+		mmap := make(map[string]interface{})
+		methods := map[string]interface{}{
+			"a": func() string { return "foo" },
+			"b": func() interface{} { return 1 },
+			"c": func() {},
+		}
+		expected := map[string]interface{}{}
+		for key, set := range methods {
+			err := setIfNotNil(mmap, key, set)
+			assert.NotNil(t, err)
+		}
+		assert.Equal(t, mmap, expected)
+	})
+}
+
 // assertHasTimestamp checks that the specified measurement has the expected ts
 func assertHasTimestamp(t *testing.T, acc testutil.Accumulator, measurement string, ts int64) {
 	expected := time.Unix(ts, 0)
